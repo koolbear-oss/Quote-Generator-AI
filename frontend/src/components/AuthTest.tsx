36eade686f6f9f68d4fbaa2cb7c66eec2ac0deb5
koolbear-oss <mattias.colla@gmail.com>
2025-10-18 22:59:12 +0200

    push

---
+++ frontend/src/components/AuthTest.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+import { AuthTest } from './components/AuthTest';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <AuthTest />
+    </div>
+  );
+}
+
+export default App;
